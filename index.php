2c8b8e1c67dad7bb2a51db3e6888f756d412efee
Muhammad Zeshan <zeshan77@gmail.com>
2023-02-20 09:59:05 +0500

    class 01: variables

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Document</title>
+        <style>
+            body {
+                display: grid;
+                place-items: center;
+                margin: 0;
+                font-family: sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+    <?php
+        $book = "Dark Matters";
+        $hasRead = 0;
+
+         if($hasRead === true) {
+            $message = "You have read Dark Matters!";
+        } else {
+             $message = "You have not read Dark Matters!";
+         }
+    ?>
+
+        <h1><?php echo $message; ?></h1>
+    </body>
+</html>

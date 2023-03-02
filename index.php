29c8adac9b4ad1ec2af8fbd5eb8420d962f12e10
Muhammad Zeshan <zeshan77@gmail.com>
2023-03-02 09:44:08 +0500

    PDO

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+$dsn = 'mysql:host=localhost;user=root;dbname=phpclass;';
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare('select * from posts limit 1;');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<ul>';
+foreach ($posts as $post) {
+    echo "<li>{$post['id']}: {$post['title']}: {$post['description']}</li>";
+}
+echo '</ul>';
+exit;
+?>
+
+
 <?php $title = 'Home'; ?>
 <?php require('partials/header.view.php'); ?>
 <?php require('partials/nav.view.php'); ?>
